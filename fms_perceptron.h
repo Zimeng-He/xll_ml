8ec5ed15262ef0490cec0b39a5d2c59260ec4350
Keith A. Lewis <kal@kalx.net>
2026-01-06 18:16:57 -0500

    compiling

--- fms_perceptron.h
+++ fms_perceptron.h
@@ -1,25 +1,38 @@
-// fms_perceptron.h
+﻿// fms_perceptron.h
 // A perceptron is a hyperplane separating two sets of points in R^n.
 // Given sets S_0 and S_1, find a vector w and a scalar b such that
 // w.x + b < 0 for x in S_0 and w.x + b > 0 for x in S_1.
 #pragma once
 
+#define MDSPAN_USE_PAREN_OPERATOR 1
 #include <experimental/mdspan>
 #include <experimental/linalg>
 
 namespace fms::perceptron {
 
-	// Update vector w and scalar b given point x and label y in {-1, 1}
-	void update(std::mdspan<double> w, double& b, 
-		const std::mdspan<const double> x, int y, double alpha = 1.0)
-	{
-		using namespace std::experimental::linalg;
+    template<class T>
+    using mdspan = std::mdspan <T, std::extents<int, std::dynamic_extent>>;
 
-		double wx_b = linalg::dot(w, x, b);
-		// w.x + b does not fit y
-		if (wx_b * y < 0) {
+    // Update vector w and scalar b given point x and label y in {-1, 1}
+    void update(mdspan<double>& w, double& b,
+        mdspan<const double> x, int y, double alpha = 1.0)
+    {
+        using std::experimental::linalg::add;
+        using std::experimental::linalg::dot;
+        using std::experimental::linalg::scaled;
 
-		}
-	}
+        // Compute w·x + b
+        double prediction = dot(w, x, b);
 
-} // namespace fms
+        // Check if misclassified
+        if (prediction * y < 0) {
+            // Update: w += alpha y x    ,
+            add(scaled(alpha * y, w), x, w);
+
+            // Update bias
+            b += alpha * y;
+        }
+    }
+
+ 
+} // namespace fms::perceptron
--- xll_ml.cpp
+++ xll_ml.cpp
@@ -1,2 +1,17 @@
-// xll_ml.cpp
+﻿// xll_ml.cpp
 #include "fms_perceptron.h"
+#include "xll24/include/xll.h"
+
+using namespace xll;
+
+// Create mdspan from FP data type.
+constexpr auto make_mdspan(_FP12* pa)
+{
+	return std::experimental::mdspan<double, std::extents<INT32, std::dynamic_extent>>
+		(pa->array, pa->rows * pa->columns);
+}
+constexpr auto make_mdspan(const _FP12* const pa)
+{
+	return std::experimental::mdspan<const double, std::extents<INT32, std::dynamic_extent>>
+		(pa->array, pa->rows * pa->columns);
+}
