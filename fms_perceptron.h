5d8d213b2a65b265af8ff1dc618f0bc4d9045da9
Keith A. Lewis <kal@kalx.net>
2026-01-10 19:10:45 -0500

    remove bias term

--- fms_perceptron.h
+++ fms_perceptron.h
@@ -4,33 +4,74 @@
 // w.x + b < 0 for x in S_0 and w.x + b > 0 for x in S_1.
 #pragma once
 
+#include <exception>
+#include <span>
 #define MDSPAN_USE_PAREN_OPERATOR 1
 #include <experimental/mdspan>
 #include <experimental/linalg>
 
 namespace fms::perceptron {
 
-    // Update vector w and scalar b given point x and label y in {-1, 1}
-    template<class T, class S, ::std::size_t E>
-    void update(std::mdspan<T, std::extents<S,E>>& w, T& b,
-        std::mdspan<const T, std::extents<S, E>> x, int y, double alpha = 1.0)
+    // Update weights w given point x and label y in {-1, 1}
+    template<class T>
+    T update(std::span<T>& _w, const std::span<T>& _x, int y, T alpha = 1.0)
     {
         using std::experimental::linalg::add;
         using std::experimental::linalg::dot;
         using std::experimental::linalg::scaled;
 
-        // Compute w·x + b
-        double prediction = dot(w, x) + b;
-
-        // Check if misclassified
-        if (prediction * y < 0) {
-            // Update: w = w + alpha y x    ,
-            add(w, scaled(alpha * y, x), w);
-
-            // Update bias
-            b += alpha * y;
+        if (_w.size() != _x.size()) {
+            throw std::invalid_argument("weight and point must have the same size");
         }
+        if (y != 1 and y != -1) {
+            throw std::invalid_argument("label must be 1 or -1");
+        }
+
+		std::mdspan w(_w.data(), _w.size());
+		std::mdspan x(_x.data(), _x.size());
+
+		double y_ = dot(w, x);
+        // Check if misclassified
+        if (y_ * y < 0) {
+            // Update: w = w + alpha dy x    ,
+            add(w, scaled(alpha * (y_ - y), x), w);
+        }
+        
+        return y_;
     }
 
+    template<class T = double>
+    struct neuron {
+        std::vector<T> w;
+
+        neuron(size_t n)
+            : w(n)
+		{ }
+        neuron(const std::vector<T>& w)
+            : w(w)
+        { }
+        neuron(const neuron&) = default;
+        neuron& operator=(const neuron&) = default;
+        neuron(neuron&&) = default;
+        neuron& operator=(neuron&&) = default;
+        ~neuron() = default;
+
+        void update(const std::span<const T>& x, int y, double alpha = 1.0)
+        {
+            if (x.size() == 0 or x[0] != 1) {
+                throw std::invalid_argument("first element of data must be 1");
+            }
+  
+            fms::perceptron::update(w, x, y, alpha);
+		}
+        
+        using pair = std::pair<const std::span<const T>, int>;
+        void train(const std::span<pair>& xy, double alpha = 1.0)
+        {
+            for (const auto [x, y] : xy) {
+                update(x, y, alpha);
+            }
+        }
+    };
  
 } // namespace fms::perceptron
--- xll_ml.cpp
+++ xll_ml.cpp
@@ -8,7 +8,6 @@ AddIn xai_perceptron_update(
 	Function(XLL_FP, L"xll_perceptron_update", L"PERCEPTRON.UPDATE")
 	.Arguments({
 		Arg(XLL_FP, L"w", L"is an array of weights"),
-		Arg(XLL_DOUBLE, L"b", L"is the bias term"),
 		Arg(XLL_FP, L"x", L"is an array representing the input vector"),
 		Arg(XLL_INT, L"y", L"is the label (-1 or 1)"),
 		Arg(XLL_DOUBLE, L"alpha", L"is the learning rate (default=1.0)", 1.0)
@@ -16,15 +15,15 @@ AddIn xai_perceptron_update(
 	.FunctionHelp(L"Update perceptron weights and bias given input vector and label.")
 	.Category(L"ML")
 );
-_FP12* WINAPI xll_perceptron_update(_FP12* pw, double b, const _FP12* const px, int y, double alpha)
+_FP12* WINAPI xll_perceptron_update(_FP12* pw, _FP12* const px, int y, double alpha)
 {
 #pragma XLLEXPORT
 	try {
-		auto w = std::mdspan(pw->array, size(*pw));
-		auto x = std::mdspan(px->array, size(*px));
 		alpha = alpha ? alpha : 1;
+		auto w = span(*pw);
+		auto x = span(*px);
 
-		fms::perceptron::update(w, b, x, y, alpha);
+		fms::perceptron::update(w, x, y, alpha);
 	}
 	catch (const std::exception& ex) {
 		XLL_ERROR(ex.what());
